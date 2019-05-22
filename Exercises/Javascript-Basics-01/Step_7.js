9a7e2cc208f565d5ac17e772faea2e920816a78a
AshrafJurdi <ashrafjurdi@gmail.com>
2019-05-22 07:33:51 +0300

    Step_7

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,11 @@
+var shoe= parseFloat(prompt("Enter the your shoe size please:"));
+var birth= parseInt(prompt("Enter your Birth year:"));
+var a= shoe*2;
+var b= a+5;
+var c= b*50;
+var d= c-birth;
+var e= d+1766;
+
+alert(e);
+
+
