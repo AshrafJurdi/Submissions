7a84d01898e0ecfb19302131439954d373fa3078
AshrafJurdi <ashrafjurdi@gmail.com>
2019-05-23 11:35:42 +0300

    Step_2

--- Exercises/Javascript-Basics-01/Step_7.js
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -1,11 +1,16 @@
 var shoe= parseFloat(prompt("Enter the your shoe size please:"));
 var birth= parseInt(prompt("Enter your Birth year:"));
-var a= shoe*2;
-var b= a+5;
-var c= b*50;
-var d= c-birth;
-var e= d+1766;
-
-alert("Result: " + e);
 
 
+
+
+function calculate() {
+    var a= shoe*2;
+    var b= a+5;
+    var c= b*50;
+    var d= c-birth;
+    var e= d+1766;
+    alert("Result: " + e);
+}
+
+calculate();
---
+++ Exercises/Javascript-Basics-02/step 2/Step_2.js
@@ -0,0 +1,9 @@
+var a = document.querySelector("#name");
+
+a.addEventListener("blur", losingfocus);
+
+
+
+
+    function losingfocus(){
+alert("thank you for participating!")}
