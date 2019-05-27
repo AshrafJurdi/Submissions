c72625d0c6eba35bc2a4f2663195012d28876cf5
AshrafJurdi <ashrafjurdi@gmail.com>
2019-05-27 14:24:38 +0300

    Step_3

---
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -0,0 +1,44 @@
+var text = document.getElementById('text');
+var green = document.querySelector('.green');
+var red = document.querySelector('.red');
+var blue = document.querySelector('.blue');
+
+
+
+
+
+
+green.onclick = function textColor(){
+    text.style.color= 'green';
+}
+red.onclick = function textColor2(){
+    text.style.color= 'red';
+}
+blue.onclick = function textColor3(){
+    text.style.color= 'blue';
+}
+
+
+   
+
+    
+
+/*green.onclick = text.style.color= 'green';
+red.onclick = text.style.color= 'red';
+blue.onclick = text.style.color= 'blue';*/
+
+
+
+/*function buttonClick(green) {
+    if (green)
+    text.style.color= 'green';
+
+    else if (red)
+    text.style.color= 'red';
+
+    else 
+    text.style.color= 'blue';
+
+
+
+}*/
