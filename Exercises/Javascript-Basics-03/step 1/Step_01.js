6eb4a80da75ee448c50b746e387722853b11cbf3
AshrafJurdi <ashrafjurdi@gmail.com>
2019-05-27 09:20:16 +0300

    Step_1

---
+++ Exercises/Javascript-Basics-03/step 1/Step_01.js
@@ -0,0 +1,9 @@
+ var v=document.getElementById('image1') ;
+ v.addEventListener('mouseover', ashraf)
+ function ashraf() {
+    v.style= "border: 3px solid red"
+ }
+ v.addEventListener('mouseout', xxx)
+ function xxx() {
+    v.style= "border: 0"
+ }
