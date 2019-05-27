e9fff9562703943f6c31e233798c8e16443b3b30
AshrafJurdi <ashrafjurdi@gmail.com>
2019-05-27 14:24:55 +0300

    Step_3

--- Exercises/Javascript-Basics-03/step 3/Step_3.js
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -17,28 +17,3 @@ red.onclick = function textColor2(){
 blue.onclick = function textColor3(){
     text.style.color= 'blue';
 }
-
-
-   
-
-    
-
-/*green.onclick = text.style.color= 'green';
-red.onclick = text.style.color= 'red';
-blue.onclick = text.style.color= 'blue';*/
-
-
-
-/*function buttonClick(green) {
-    if (green)
-    text.style.color= 'green';
-
-    else if (red)
-    text.style.color= 'red';
-
-    else 
-    text.style.color= 'blue';
-
-
-
-}*/
