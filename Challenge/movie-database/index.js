18191110821729027cd457399f449503e708d681
AshrafJurdi <ashrafjurdi@gmail.com>
2019-06-11 19:50:22 +0300

    Let's complicate the API

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -11,3 +11,19 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.get('/test', (req, res) => res.send({status:200, message:"ok"}))
 
 app.get('/time', (req, res) => res.send({status:200, message: time}))
+
+app.get('/hello/:ID?', (req, res) => { 
+    if (req.params.ID !== undefined){
+    res.send({status:200, message:"Hello, " +req.params.ID})
+}else {
+    res.send({status:200, message:"Hello" })
+}})
+
+app.get('/search', (req, res) => { 
+    if (req.query.s !== undefined && req.query.s !== "") {
+    res.send({status:200, message:"ok", data:req.query.s})
+}else {
+    res.send({status:500, error:true, message:"you have to provide a search"})
+}})
+
+    
